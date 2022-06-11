1055d43ca04b2430fae44bc5cb2de79ce132ce5a
Artifacting <92096499+ArtifactingMC@users.noreply.github.com>
2022-06-10 20:27:19 -0400

    performance?? maybe?
    e

--- src/commands/utils/networkinfo.js
+++ src/commands/utils/networkinfo.js
@@ -32,85 +32,86 @@ module.exports = {
             advanced = false
         }
 
-        let response = await fetch(`https://api.minehut.com/network/players/distribution`)
-        let json = await response.json()
+        if (advanced === true) {
+            let response = await fetch(`https://api.minehut.com/network/players/distribution`)
+            let json = await response.json()
 
-        const javaTotal = json['javaTotal']
-        const javaLobby = json['javaLobby']
-        const javaPlayerServer = json['javaPlayerServer']
-        const bedrockTotal = json['bedrockTotal']
-        const bedrockLobby = json['bedrockLobby']
-        const bedrockPlayerServer = json['bedrockPlayerServer']
+            const javaTotal = json['javaTotal']
+            const javaLobby = json['javaLobby']
+            const javaPlayerServer = json['javaPlayerServer']
+            const bedrockTotal = json['bedrockTotal']
+            const bedrockLobby = json['bedrockLobby']
+            const bedrockPlayerServer = json['bedrockPlayerServer']
 
-        const totalPlayers = javaTotal + bedrockTotal
-        const totalLobbyPlayers = javaLobby + bedrockLobby
-        const totalServerPlayers = javaPlayerServer + bedrockPlayerServer
-
-
-        response = await fetch(`https://api.minehut.com/network/simple_stats`)
-        json = await response.json()
-
-        const server_max = json['server_max']
-        const server_count = json['server_count']
+            const totalPlayers = javaTotal + bedrockTotal
+            const totalLobbyPlayers = javaLobby + bedrockLobby
+            const totalServerPlayers = javaPlayerServer + bedrockPlayerServer
 
 
-        // 
-        // Thanks to chrishours#0001 on Discord for help with the ratio calculations
-        // 
-        const javaBedrockRatio = Math.round(javaTotal / totalPlayers * 100)
-        const javaBedrockServerRatio = Math.round(javaPlayerServer / totalServerPlayers * 100)
-        const javaBedrockLobbyRatio = Math.round(javaLobby / totalLobbyPlayers * 100)
+            response = await fetch(`https://api.minehut.com/network/simple_stats`)
+            json = await response.json()
+
+            const server_max = json['server_max']
+            const server_count = json['server_count']
+
+
+            //
+            // Thanks to chrishours#0001 on Discord for help with the ratio calculations
+            //
+            const javaBedrockRatio = Math.round(javaTotal / totalPlayers * 100)
+            const javaBedrockServerRatio = Math.round(javaPlayerServer / totalServerPlayers * 100)
+            const javaBedrockLobbyRatio = Math.round(javaLobby / totalLobbyPlayers * 100)
 
 
 
 
-        function graphData() {
+            function graphData() {
 
-            const chart = new QuickChart();
-            chart.setConfig({
-                type: 'bar',
-                data: {
-                    labels: ['Total', 'Servers', 'Lobbies'],
-                    datasets: [
-                        {
-                            label: 'Java',
-                            backgroundColor: "rgb(255, 99, 132)",
-                            borderColor: "rgb(255, 99, 132)",
-                            data: [
-                                javaTotal,
-                                javaPlayerServer,
-                                javaLobby
-                            ],
-                        },
-                        {
-                            label: 'Bedrock',
-                            backgroundColor: "rgb(54, 162, 235)",
-                            borderColor: "rgb(54, 162, 235)",
-                            data: [
-                                bedrockTotal,
-                                bedrockPlayerServer,
-                                bedrockLobby
-                            ]
-                        }
-                    ]
-                },
-                options: {
-                    "stacked": false,
-                    "title": {
-                        "display": true,
-                        "text": "Minehut Network Player Statistics"
+                const chart = new QuickChart();
+                chart.setConfig({
+                    type: 'bar',
+                    data: {
+                        labels: ['Total', 'Servers', 'Lobbies'],
+                        datasets: [
+                            {
+                                label: 'Java',
+                                backgroundColor: "rgb(255, 99, 132)",
+                                borderColor: "rgb(255, 99, 132)",
+                                data: [
+                                    javaTotal,
+                                    javaPlayerServer,
+                                    javaLobby
+                                ],
+                            },
+                            {
+                                label: 'Bedrock',
+                                backgroundColor: "rgb(54, 162, 235)",
+                                borderColor: "rgb(54, 162, 235)",
+                                data: [
+                                    bedrockTotal,
+                                    bedrockPlayerServer,
+                                    bedrockLobby
+                                ]
+                            }
+                        ]
                     },
-                }
-            });
+                    options: {
+                        "stacked": false,
+                        "title": {
+                            "display": true,
+                            "text": "Minehut Network Player Statistics"
+                        },
+                    }
+                });
 
 
-            return chart.getUrl()
-        }
+                return chart.getUrl()
+            }
 
 
 
 
-        if (advanced) {
+
             let advancedEmbed = new MessageEmbed()
                 .setColor('GREEN')
                 .setTitle(`Minehut Statistics (Advanced)`)
@@ -133,15 +134,30 @@ module.exports = {
 
             await interaction.reply({ embeds: [advancedEmbed], ephemeral: hidden });
         } else {
-            let simpleEmbed = new MessageEmbed()
-                .setColor('GREEN')
-                .setTitle(`Minehut Statistics`)
-                .setThumbnail('https://github.com/ArtifactingMC/mhstatsbot-assets/raw/main/mhlogo.png')
-                .addField('Total Players', `${totalPlayers}`, true)
-                .addField('Total Servers', `${server_count}`, true)
-                .setFooter({ text: `Minehut Utils ${process.env.VERSION}`, iconURL: 'https://github.com/ArtifactingMC/mhstatsbot-assets/raw/main/logo.png'})
-                .setTimestamp()
-            await interaction.reply({ embeds: [simpleEmbed], ephemeral: hidden });
+
+            let response = await fetch(`https://api.minehut.com/servers`)
+            let json = await response.json()
+
+            try {
+
+                let totalPlayers = json['total_players']
+                let server_count = json['total_servers']
+
+                let simpleEmbed = new MessageEmbed()
+                    .setColor('GREEN')
+                    .setTitle(`Minehut Statistics`)
+                    .setThumbnail('https://github.com/ArtifactingMC/mhstatsbot-assets/raw/main/mhlogo.png')
+                    .addField('Total Players', `${totalPlayers}`, true)
+                    .addField('Total Servers', `${server_count}`, true)
+                    .setFooter({
+                        text: `Minehut Utils ${process.env.VERSION}`,
+                        iconURL: 'https://github.com/ArtifactingMC/mhstatsbot-assets/raw/main/logo.png'
+                    })
+                    .setTimestamp()
+                await interaction.reply({embeds: [simpleEmbed], ephemeral: hidden});
+            } catch (e) {
+                await interaction.reply({content: "An error occured while fetching information!", ephemeral: true});
+            }
         }
     }
 };
