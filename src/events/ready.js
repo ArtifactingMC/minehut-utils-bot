418a9c374c5084f193b2928a3961d6c02e128b20
Artifacting <92096499+ArtifactingMC@users.noreply.github.com>
2022-06-11 12:28:36 -0400

    personal update (ignore)

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,6 +6,7 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log(`${client.user.username} logged in!`);
+        console.log('Bot has started successfully!') // To make my host solution mark the bot as online lmao
 
         client.user.setActivity(`Minehut API`, ({type: "WATCHING"}))
 
